e702b9b169314af465a0f12b63640f9dd2533001
markoburda <47101243+markoburda@users.noreply.github.com>
2019-05-18 13:02:43 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask_cors import CORS
-from flask import request, render_template, jsonify, json, Flask, send_from_directory
+from flask import Flask, request, render_template, json, jsonify, send_from_directory
 import json
-import numpy
 import cv2
+import numpy as np
 import io
 
 app = Flask(__name__)
@@ -35,7 +35,7 @@ def load_shards(path):
 def preprocessing(file):
     in_memory_file = io.BytesIO()
     file.save(in_memory_file)
-    data = numpy.fromstring(in_memory_file.getvalue(), dtype=numpy.uint8)
+    data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
     img = cv2.imdecode(data, 0)
     res = cv2.resize(img, dsize=(28, 28), interpolation=cv2.INTER_CUBIC)
     return res
